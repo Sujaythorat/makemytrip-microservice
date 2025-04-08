6766a370652c0f898cbbca152c8fbb73cd714b0d
Siddharth Thorat <sujay294@gmail.com>
2025-04-08 17:10:36 +0530

    code refactor

---
+++ src/main/java/com/example/makemytrip_microservice/Flight.java
@@ -0,0 +1,10 @@
+package com.example.makemytrip_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Flight {   @GetMapping ("/Flight")
+    public String getData () { return "Please book your flight tickets";
+}}
